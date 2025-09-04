1f17a0026544c26c3777be9f625daeb749b1ee0d
Gourav-Dutta <duttagourav047@gmail.com>
2025-09-04 22:40:50 +0530

    jsguygsj

--- frontend/src/components/AuthPageLayout.jsx
+++ frontend/src/components/AuthPageLayout.jsx
@@ -7,7 +7,6 @@ export function AuthPageLayout(){
   return (
    <div className="flex items-center justify-center bg-gray-100 min-h-screen w-full">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
-          <Outlet />
       </div>
     </div>
 
