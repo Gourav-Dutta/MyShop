981102eeef46ac43cf181a7ffab7ca2764b86440
Gourav Dutta <duttagourav047@gmail.com>
2025-10-26 23:29:57 +0530

    Comment out logs

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -5,10 +5,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-console.log("Cloudinary config test:");
-console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API key:", process.env.CLOUDINARY_API_KEY);
-console.log("API secret:", process.env.CLOUDINARY_API_SECRET ? "Loaded ✅" : "Missing ❌");
+// console.log("Cloudinary config test:");
+// console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("API key:", process.env.CLOUDINARY_API_KEY);
+// console.log("API secret:", process.env.CLOUDINARY_API_SECRET ? "Loaded ✅" : "Missing ❌");
 
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
